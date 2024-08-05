c482c0611967e485952fc4ac177b01fa21a46fb9
林钟一六 <1477007136@qq.com>
2024-08-05 18:12:13 +0800

    升级版本到 0.2.1

--- mybatis-flex-reactor-spring/build.gradle.kts
+++ mybatis-flex-reactor-spring/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.juxest"
-version = "0.2"
+version = "0.2.1"
 
 repositories {
     mavenLocal()
@@ -13,7 +13,7 @@ repositories {
 
 dependencies {
     compileOnly("com.mybatis-flex:mybatis-flex-core:1.9.5")
-    api("com.juxest:mybatis-flex-reactor-core:0.2")
+    api("com.juxest:mybatis-flex-reactor-core:0.2.1")
     // SpringBoot 注解
     compileOnly("org.springframework:spring-beans:6.1.8")
 }
