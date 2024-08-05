0907f3d7941f728bb4ebd95b4b73ffe3bddb75c9
林钟一六 <1477007136@qq.com>
2024-08-05 10:05:03 +0800

    完善了 ServiceImpl 的注释

--- mybatis-flex-reactor-spring/src/main/java/com/mybatis/flex/reactor/spring/ReactorServiceImpl.java
+++ mybatis-flex-reactor-spring/src/main/java/com/mybatis/flex/reactor/spring/ReactorServiceImpl.java
@@ -5,12 +5,23 @@ import com.mybatisflex.core.BaseMapper;
 import com.mybatisflex.core.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * ReactorService 实现类
+ * @param <Mapper> Mapper
+ * @param <Entity> 实体类型
+ */
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 public class ReactorServiceImpl<Mapper extends BaseMapper<Entity>, Entity> implements ReactorService<Entity> {
 
+    /**
+     * 外部注入 Mapper
+     */
     @Autowired
     protected Mapper mapper;
 
+    /**
+     * 外部注入 IService 实现
+     */
     @Autowired(required = false)
     protected IService<Entity> blockService;
 
@@ -19,10 +30,6 @@ public class ReactorServiceImpl<Mapper extends BaseMapper<Entity>, Entity> imple
         return mapper;
     }
 
-    /**
-     * 获取阻塞式非响应式 Service。默认会注入已有的 IService 实现，如果没有则会尝试创建一个默认的 IService 实现。
-     * @return IService 实现对象
-     */
     @Override
     public IService<Entity> getBlockService() {
         if (blockService == null) {
